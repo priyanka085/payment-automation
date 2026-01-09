0f6ebc8ca637c2ed83a5cad4a3b8c03f2465e765
Priyanka <priyanka.kundu.sahoo@gmail.com>
2026-01-09 17:29:43 -0500

    modified api_client.py

--- utils/api_client.py
+++ utils/api_client.py
@@ -4,7 +4,7 @@ class PaymentClient:
     def __init__(self, base_url):
         self.base_url = base_url
 
-    def authorize_payement(self, payload, headers):
+    def authorize_payment(self, payload, headers):
         return request.post(
             f"{self.base_url}/authorize",
             json=payload,
