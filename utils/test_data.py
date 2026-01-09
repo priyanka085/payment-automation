9d5feb4457f929752a0c1862aa5f45e2f57f5658
Priyanka <priyanka.kundu.sahoo@gmail.com>
2026-01-09 17:08:28 -0500

    coded test_data.py

---
+++ utils/test_data.py
@@ -0,0 +1,15 @@
+VALID_PAYMENT = {
+    "cardNumber": "4111111111111111",
+    "expiry": "12/26",
+    "cvv": "123",
+    "amount": 100,
+    "currency": "USD"
+}
+
+INVALID_PAYMENT = {
+    "cardNumber": "4000000000000002",
+    "expiry": "12/20",
+    "cvv": "000",
+    "amount": 100,
+    "currency": "USD"
+}
