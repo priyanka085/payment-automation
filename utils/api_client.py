e1608128de2b40788a4d566f54cdb460bd2bd938
Priyanka <priyanka.kundu.sahoo@gmail.com>
2026-01-09 17:04:02 -0500

    code written api_client.py

---
+++ utils/api_client.py
@@ -0,0 +1,12 @@
+import request
+
+class PaymentClient:
+    def __init__(self, base_url):
+        self.base_url = base_url
+
+    def authorize_payement(self, payload, headers):
+        return request.post(
+            f"{self.base_url}/authorize",
+            json=payload,
+            headers=headers
+        )
