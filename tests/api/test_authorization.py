26b9e4cd32a6dd2be949b62da921818fba945cec
Priyanka <priyanka.kundu.sahoo@gmail.com>
2026-01-12 23:23:01 -0500

    updated test suit

--- tests/api/test_authorization.py
+++ tests/api/test_authorization.py
@@ -1,6 +1,6 @@
 import pytest
 import responses
-from utils.test_data import VALID_PAYMENT
+from utils.test_data import *
 
 @responses.activate
 @pytest.mark.smoke
@@ -25,3 +25,22 @@ def test_payment_authorization_success(payment_client):
 
     assert response.status_code == 200
     assert response.json()["status"] == "APPROVED"
+
+def test_payment_authorization_failure(payment_client):
+    responses.add(
+        responses.POST,
+        "https://mock-gateway/authorize",
+        json={
+            "status": "REJECTED",
+            "transaction id": "TXN124",
+            "amount": 100,
+            "currency": "USD"
+        },
+        status=422
+    )
+    response = payment_client.authorize_payment(
+        payload=INVALID_PAYMENT,
+        headers={"Content-Type": "application/json"}
+    )
+    assert response.status_code == 422
+    assert response.json()["status"]=="REJECTED"
